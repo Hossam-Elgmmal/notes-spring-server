6a01790e7ca564609c94c6394e49ed9cebba19bd
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2025-05-27 09:07:02 +0300

    added authentication and notes logic

---
+++ src/main/kotlin/com/spring/notes/controllers/NoteController.kt
@@ -0,0 +1,91 @@
+package com.spring.notes.controllers
+
+import com.spring.notes.controllers.NoteController.NoteResponse
+import com.spring.notes.database.model.Note
+import com.spring.notes.database.repository.NotesRepository
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import org.bson.types.ObjectId
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NotesRepository
+) {
+
+    data class NoteRequest(
+        val id: String?,
+        @Valid @NotBlank(message = "Title cannot be blank.")
+        val title: String,
+        val content: String,
+        val color: Long,
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant,
+    )
+
+    @PostMapping
+    fun save(
+        @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val ownerId = SecurityContextHolder.getContext().authentication.principal as String
+
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId(ownerId)
+            )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(): List<NoteResponse> {
+        val ownerId = SecurityContextHolder.getContext().authentication.principal as String
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+
+    @DeleteMapping(path = ["/{id}"])
+    fun deleteById(
+        @PathVariable id: String
+    ) {
+        val note = repository.findById(ObjectId(id)).orElseThrow {
+            IllegalArgumentException("Note not found")
+        }
+        val ownerId = SecurityContextHolder.getContext().authentication.principal as String
+        if (note.ownerId.toHexString() == ownerId) {
+            repository.deleteById(ObjectId(id))
+        }
+    }
+}
+
+private fun Note.toResponse() : NoteController.NoteResponse{
+    return NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt,
+    )
+}
---
+++ src/main/kotlin/com/spring/notes/database/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.spring.notes.database.repository
+
+import com.spring.notes.database.model.User
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface UserRepository: MongoRepository<User, ObjectId> {
+    fun findByEmail(email: String): User?
+}
